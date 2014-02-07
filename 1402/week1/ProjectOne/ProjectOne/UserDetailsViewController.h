b8b97d01db8a8a5695913045fd77d0f80cf1f569
Wesley Seago <wseago2@fullsail.edu>
2014-02-06 23:34:46 -0500

    Week 1 complete.

---
+++ 1402/week1/ProjectOne/ProjectOne/UserDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserDetailsViewController.h
+//  ProjectOne
+//
+//  Created by Wesley Seago on 2/6/14.
+//  Copyright (c) 2014 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserDetailsViewController : UIViewController
+
+@end
---
+++ 1402/week1/ProjectOne/ProjectOne/UserDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  UserDetailsViewController.m
+//  ProjectOne
+//
+//  Created by Wesley Seago on 2/6/14.
+//  Copyright (c) 2014 Wesley Seago. All rights reserved.
+//
+
+#import "UserDetailsViewController.h"
+
+@interface UserDetailsViewController ()
+
+@end
+
+@implementation UserDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
