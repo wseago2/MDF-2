b1c5c7c038f9e82275aca85e54e33410b14780a3
Wesley Seago <wseago2@fullsail.edu>
2014-02-24 08:51:58 -0500

    Project4 setup.

--- 1402/week4/Project4/Project4/ViewController.m
+++ 1402/week4/Project4/Project4/ViewController.m
@@ -16,6 +16,13 @@
 
 - (void)viewDidLoad
 {
+    //MovieInfo custom objects
+    
+    //TheaterInfo custom objects
+    
+    //TheaterInfoObjects array
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -24,6 +31,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma tableView methods
+-(int)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"tableCell"];
+    if (cell != nil)
+    {
+    return cell;
+    }
+    
+    return nil;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectMake (0.0f, 0.0f, myTableview.frame.size.width, 30.0f)];
+    if (headerLabel != nil)
+    {
+        headerLabel.backgroundColor = [UIColor redColor];
+        headerLabel.text = @"This Theater";
+    }
+    return headerLabel;
 }
 
 @end
